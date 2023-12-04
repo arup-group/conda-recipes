fa4a1f888e7d1b8654dbeb45b8b116a693bf7ebb
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-12-04 10:19:55 +0000

    Update windows

--- xml2/bld.bat
+++ xml2/bld.bat
@@ -1,15 +1,9 @@
 copy %RECIPE_DIR%\unistd.h %SRC_DIR% || exit 1
 
-mkdir build
-cd build
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      %SRC_DIR%
-if errorlevel 1 exit 1
-
-nmake xml2 || exit 1
-nmake 2csv || exit 1
+echo "xml2"
+make "xml2"
+echo "2csv"
+make "2csv"
 
 mkdir -p %PREFIX/Library/bin || exit 1
 copy "xml2" "2csv" %PREFIX/Library/bin || exit 1
