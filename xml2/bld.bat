312b08aa2dca8a57ee740495292ea29850a39721
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-12-04 14:25:37 +0000

    Update build bat

--- xml2/bld.bat
+++ xml2/bld.bat
@@ -3,6 +3,6 @@ set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 set INC=xml2-config --cflags
 
-make --trace "xml2"
-make "2csv"
+echo xml2-config --cflags
+make --trace all
 copy "xml2" "2csv" %LIBRARY_BIN%
