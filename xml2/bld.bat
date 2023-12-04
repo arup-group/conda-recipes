63e04a7237b3a9eae705c3a997ad4a27c4a14051
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-12-04 10:44:20 +0000

    Revert

--- xml2/bld.bat
+++
@@ -1,9 +0,0 @@
-copy %RECIPE_DIR%\unistd.h %SRC_DIR% || exit 1
-
-echo "xml2"
-make "xml2"
-echo "2csv"
-make "2csv"
-
-mkdir -p %PREFIX/Library/bin || exit 1
-copy "xml2" "2csv" %PREFIX/Library/bin || exit 1
--- xml2/build.sh
+++
@@ -1,5 +0,0 @@
-make xml2 2csv
-
-mkdir -p $PREFIX/bin
-
-cp "xml2" "2csv" $PREFIX/bin
