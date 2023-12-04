9615e630cec2b8874880ca8fbbe422849312ceb3
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-12-04 17:14:14 +0000

    Update bld.bat

--- xml2/bld.bat
+++ xml2/bld.bat
@@ -3,6 +3,6 @@ set LIBRARY_INC=%LIBRARY_INC%\libxml2;%LIBRARY_INC%
 
 echo %LIBRARY_INC%
 
-make -lxml2 "xml2"
-make -lxml2 "2csv"
+cl "xml2.c" /I %LIBRARY_INC%\libxml2
+make /link lxml2 "2csv"
 copy "xml2" "2csv" %LIBRARY_BIN%
